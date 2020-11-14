7d62a5a61ed2120467e306a70763fe4aba2ef2e9
chop1k <fakeshopendesu@gmail.com>
2020-11-14 22:08:44 +0300

    added comments

--- src/Argument.php
+++ src/Argument.php
@@ -2,6 +2,11 @@
 
 namespace Consolly\Argument;
 
+/**
+ * Class Argument contains main functional for working with console arguments
+ *
+ * @package Consolly\Argument
+ */
 class Argument
 {
     public const Option = 100;
@@ -9,9 +14,16 @@ class Argument
     public const Value = 200;
     public const Command = 300;
 
+    /**
+     * Contains raw argument
+     *
+     * @var string $arg
+     */
     protected string $arg;
 
     /**
+     * Returns raw argument
+     *
      * @return string
      */
     public function getArg(): string
@@ -20,6 +32,8 @@ class Argument
     }
 
     /**
+     * Returns raw argument
+     *
      * @param string $arg
      */
     public function setArg(string $arg): void
@@ -27,9 +41,16 @@ class Argument
         $this->arg = $arg;
     }
 
+    /**
+     * Contains argument name or false if not specified
+     *
+     * @var false|string $name
+     */
     protected string $name;
 
     /**
+     * Returns argument name
+     *
      * @return string
      */
     public function getName(): string
@@ -38,6 +59,8 @@ class Argument
     }
 
     /**
+     * Contains argument name
+     *
      * @param string $name
      */
     public function setName(string $name): void
@@ -45,9 +68,16 @@ class Argument
         $this->name = $name;
     }
 
+    /**
+     * Contains argument value or false if value not specified
+     *
+     * @var false|string $value
+     */
     protected string $value;
 
     /**
+     * Returns argument value
+     *
      * @return string
      */
     public function getValue(): string
@@ -56,6 +86,8 @@ class Argument
     }
 
     /**
+     * Sets argument value
+     *
      * @param string $value
      */
     public function setValue(string $value): void
@@ -63,9 +95,16 @@ class Argument
         $this->value = $value;
     }
 
+    /**
+     * Contains argument position
+     *
+     * @var int|null $position
+     */
     protected ?int $position;
 
     /**
+     * Returns argument position
+     *
      * @return int|null
      */
     public function getPosition(): ?int
@@ -74,6 +113,8 @@ class Argument
     }
 
     /**
+     * Sets argument position
+     *
      * @param int|null $position
      */
     public function setPosition(?int $position): void
@@ -81,9 +122,16 @@ class Argument
         $this->position = $position;
     }
 
+    /**
+     * Contains argument type
+     *
+     * @var int $type
+     */
     protected int $type;
 
     /**
+     * Returns argument type
+     *
      * @return int
      */
     public function getType(): int
@@ -92,6 +140,8 @@ class Argument
     }
 
     /**
+     * Sets argument type
+     *
      * @param int $type
      */
     public function setType(int $type): void
@@ -99,6 +149,11 @@ class Argument
         $this->type = $type;
     }
 
+    /**
+     * Contains is option is abbreviated
+     *
+     * @var bool $abbreviated
+     */
     protected bool $abbreviated;
 
     /**
@@ -110,6 +165,8 @@ class Argument
     }
 
     /**
+     * Sets is option is abbreviated
+     *
      * @param bool $abbreviated
      */
     public function setAbbreviated(bool $abbreviated): void
@@ -126,6 +183,12 @@ class Argument
         $this->position = null;
     }
 
+    /**
+     * Builds argument by given data
+     *
+     * @return string
+     * Returns structured argument
+     */
     public function build(): string
     {
         $value = ($this->isAbbreviated()) ? '-' : '--';
@@ -148,6 +211,15 @@ class Argument
         return $this->value;
     }
 
+    /**
+     * Shortcut which parses given string and returns argument instance with parsed data
+     *
+     * @param string $argument
+     * String for parse
+     *
+     * @return Argument
+     * Instance for argument with parsed data
+     */
     public static function parse(string $argument): Argument
     {
         $arg = new Argument();
@@ -191,6 +263,19 @@ class Argument
         return $arg;
     }
 
+    /**
+     * Splits given string and returns array of chars
+     *
+     * @param string $option
+     * String for split
+     *
+     * @param bool $prefix
+     * If true then prefix '-' will be added for each char
+     *
+     * @return array
+     * Array of char.
+     * If prefix true then each item of array will be with prefix
+     */
     public static function toAbbreviations(string $option, bool $prefix = false): array
     {
         $options = str_split(trim($option, '-'));
@@ -205,11 +290,25 @@ class Argument
         return $options;
     }
 
+    /**
+     * Converts given string o option
+     *
+     * @param string $name
+     * @return string
+     * Returns option as string
+     */
     public static function toOption(string $name): string
     {
         return sprintf('--%s', trim($name, "\"'-"));
     }
 
+    /**
+     * Converts given string to abbreviated option
+     *
+     * @param string $name
+     * @return string
+     * Returns option as string
+     */
     public static function toAbbreviation(string $name): string
     {
         $name = trim($name, "\"'-");
@@ -222,21 +321,45 @@ class Argument
         return sprintf('-%s', $name[0]);
     }
 
+    /**
+     * Checks if given string is option
+     *
+     * @param string $option
+     * @return bool
+     */
     public static function isOption(string $option): bool
     {
         return strpos($option, "-") === 0 || strpos($option, "--") === 0;
     }
 
+    /**
+     * Checks if given string is abbreviated option
+     *
+     * @param string $option
+     * @return bool
+     */
     public static function isAbbreviation(string $option): bool
     {
         return strpos($option, "-") === 0 && strpos($option, "--") === false;
     }
 
+    /**
+     * Checks if given string is array of abbreviated options
+     *
+     * @param string $option
+     * @return bool
+     */
     public static function isAbbreviations(string $option): bool
     {
         return self::isAbbreviation($option) && strlen($option) > 2;
     }
 
+    /**
+     * Checks if given string is value
+     *
+     * @param string $value
+     * @return bool
+     */
     public static function isValue(string $value): bool
     {
         $len = strlen($value)-1;
@@ -250,8 +373,14 @@ class Argument
             || ($value[0] === "'" && $value[$len] === "'");
     }
 
+    /**
+     * Checks is given string is a command
+     *
+     * @param string $command
+     * @return bool
+     */
     public static function isCommand(string $command): bool
     {
-        return !self::isValue($command);
+        return !self::isValue($command) && !self::isOption($command);
     }
 }
